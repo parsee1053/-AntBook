f5acf4ecaa2b895ac1bb94b5160cd8dfbcd61b67
parsee1053 <s.naiki.parsee1053@gmail.com>
2021-12-13 00:00:16 +0900

    再帰の例題

---
+++ 2-1/fib.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	fmt.Println(fib(n))
+}
